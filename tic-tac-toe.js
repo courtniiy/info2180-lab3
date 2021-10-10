87ef5c73b9d1d5aea20019b0e51103d6e1c616fd
Courtney Brown <courtnhey.brown@gmail.com>
2021-10-10 04:35:01 -0500

    Initial commit modified and click event added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,33 @@
+window.onload = function(){
+    tic_tac_toe()
+};
 
-window.onload = function() {myFunction()};
+
+function tic_tac_toe(){
+   let board = document.querySelector('#board');
+   let tiles = board.querySelectorAll('div');
+
+   let cur_player = 'O';
 
 
-function myFunction(){
-    let el_list = document.getElementById('board');
-    let add_class = el_list.querySelectorAll('div');
-    
-    add_class.forEach(element => element.setAttribute('class', 'square'));
+   for(let tile of tiles) {
+       tile.setAttribute('class', 'square')
+
+       tile.onclick = function on_click(){
+
+        if (cur_player === 'X'){
+            tile.innerText = 'O';
+            tile.classList.add('O');
+            cur_player = 'O';
+        }
+        else if (cur_player === 'O'){
+            tile.innerText = 'X';
+            tile.classList.add('X');
+            cur_player = 'X';
+        }
+       }
+   }
+   
+   
+
 }
